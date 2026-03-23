e830e2221c4a2c5aa98aa209bf38dcd3a3ca4622
Anastasiya <mufuts@gmail.com>
2026-03-23 22:15:49 +1000

    Playlist Maker, sprint #9 (#3)
    * Playlist Maker, sprint #8
    
    * Playlist Maker, sprint #8
    
    * Исправлены отступы иконок на экране настроек, изменены названия иконок
    
    * Спринт №9
    
    * Спринт №10

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,20 +1,173 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.LinearLayout
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.appbar.MaterialToolbar
 
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: MaterialToolbar
+    private lateinit var searchEditText: EditText
+    private lateinit var clearIcon: ImageView
+    private lateinit var trackRecyclerView: RecyclerView
+    private lateinit var trackAdapter: TrackAdapter
+    private var searchText: String = ""
+    private var tracks: List<Track> = listOf()
+
+    companion object {
+        private const val SEARCH_TEXT_KEY = "SEARCH_TEXT"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_search)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+
+        val searchLayout = findViewById<LinearLayout>(R.id.searchLayout)
+        ViewCompat.setOnApplyWindowInsetsListener(searchLayout) { view, insets ->
+            val statusBar = insets.getInsets(WindowInsetsCompat.Type.statusBars())
+            val navigationBar = insets.getInsets(WindowInsetsCompat.Type.navigationBars())
+            view.updatePadding(
+                top = statusBar.top,
+                bottom = navigationBar.bottom
+            )
             insets
+        }
+
+        toolbar = findViewById(R.id.toolbar)
+        searchEditText = findViewById(R.id.searchEditText)
+        clearIcon = findViewById(R.id.clearIcon)
+        trackRecyclerView = findViewById(R.id.trackRecyclerView)
+
+        createTrackList()
+
+        trackAdapter = TrackAdapter(tracks)
+        trackRecyclerView.adapter = trackAdapter
+
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
+
+        if (savedInstanceState != null) {
+            searchText = savedInstanceState.getString(SEARCH_TEXT_KEY, "")
+            searchEditText.setText(searchText)
+            clearIcon.isVisible = searchText.isNotEmpty()
+        }
+
+        clearIcon.setOnClickListener {
+            searchEditText.text.clear()
+            searchText = ""
+            clearIcon.isVisible = false
+            hideKeyboard()
+        }
+
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                searchText = s?.toString() ?: ""
+                clearIcon.isVisible = !s.isNullOrEmpty()
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        searchEditText.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                showKeyboard()
+            }
+        }
+
+        searchEditText.setOnClickListener {
+            searchEditText.requestFocus()
+            showKeyboard()
+        }
+
+        searchEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                performSearch()
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    private fun createTrackList() {
+        tracks = listOf(
+            Track(
+                trackName = "Smells Like Teen Spirit",
+                artistName = "Nirvana",
+                trackTime = "5:01",
+                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
+            ),
+            Track(
+                trackName = "Billie Jean",
+                artistName = "Michael Jackson",
+                trackTime = "4:35",
+                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
+            ),
+            Track(
+                trackName = "Stayin' Alive",
+                artistName = "Bee Gees",
+                trackTime = "4:10",
+                artworkUrl100 = "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
+            ),
+            Track(
+                trackName = "Whole Lotta Love",
+                artistName = "Led Zeppelin",
+                trackTime = "5:33",
+                artworkUrl100 = "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
+            ),
+            Track(
+                trackName = "Sweet Child O'Mine",
+                artistName = "Guns N' Roses",
+                trackTime = "5:03",
+                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+            )
+        )
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_TEXT_KEY, searchText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchText = savedInstanceState.getString(SEARCH_TEXT_KEY, "")
+        searchEditText.setText(searchText)
+        clearIcon.isVisible = searchText.isNotEmpty()
+    }
+
+    private fun showKeyboard() {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(searchEditText, InputMethodManager.SHOW_IMPLICIT)
+    }
+
+    private fun hideKeyboard() {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(searchEditText.windowToken, 0)
+    }
+
+    private fun performSearch() {
+        val query = searchEditText.text.toString()
+        if (query.isNotEmpty()) {
+            Toast.makeText(this, query, Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,20 +1,73 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import androidx.core.net.toUri
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+import com.google.android.material.textview.MaterialTextView
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-
-        toolbar.setNavigationIcon(R.drawable.ic_back_arrow_24)
-        toolbar.setNavigationOnClickListener {
-            finish()
+        val settingsMain = findViewById<LinearLayout>(R.id.settingsMain)
+        ViewCompat.setOnApplyWindowInsetsListener(settingsMain) { view, insets ->
+            val statusBar = insets.getInsets(WindowInsetsCompat.Type.statusBars())
+            val navigationBar = insets.getInsets(WindowInsetsCompat.Type.navigationBars())
+            view.updatePadding(
+                top = statusBar.top,
+                bottom = navigationBar.bottom
+            )
+            insets
         }
+
+        val shareTextView = findViewById<MaterialTextView>(R.id.shareTextView)
+        val supportTextView = findViewById<MaterialTextView>(R.id.supportTextView)
+        val termsTextView = findViewById<MaterialTextView>(R.id.termsTextView)
+
+
+        shareTextView.setOnClickListener {
+            shareApp()
+        }
+
+        supportTextView.setOnClickListener {
+            writeToSupport()
+        }
+
+        termsTextView.setOnClickListener {
+            openTermsOfUse()
+        }
+    }
+
+    private fun shareApp() {
+        val shareIntent = Intent(Intent.ACTION_SEND).apply {
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, getString(R.string.share_message))
+        }
+        startActivity(Intent.createChooser(shareIntent, getString(R.string.share_app)))
+    }
+
+    private fun writeToSupport() {
+        val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
+            data = "mailto:".toUri()
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_email_address)))
+            putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_email_subject))
+            putExtra(Intent.EXTRA_TEXT, getString(R.string.support_email_text))
+        }
+        startActivity(emailIntent)
+    }
+
+    private fun openTermsOfUse() {
+        val termsUrl = getString(R.string.terms_url)
+        val browserIntent = Intent(Intent.ACTION_VIEW, termsUrl.toUri())
+        startActivity(browserIntent)
     }
 }
