80665c240f5c351c2cd09e3fc0e9a4eea8d5ce19
Anastasiya <mufuts@gmail.com>
2026-05-10 16:13:24 +1000

    Спринт №13

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -1,9 +1,21 @@
 package com.example.playlistmaker
 
+import java.io.Serializable
+
 data class Track(
     val trackName: String,
     val artistName: String,
     val trackTimeMillis: Long,
     val artworkUrl100: String,
-    val trackId: Int
-)
+    val trackId: Int,
+    val collectionName: String? = null,
+    val releaseDate: String? = null,
+    val primaryGenreName: String? = null,
+    val country: String? = null
+) : Serializable {
+    fun getCoverArtwork() = artworkUrl100.replaceAfterLast('/', "512x512bb.jpg")
+
+    fun getYear(): String? {
+        return releaseDate?.take(4)
+    }
+}
